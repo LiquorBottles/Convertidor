3bbce08cd082054ded0561384d23acf40f4eba52
LiquorBottler <jorubengariza@gmail.com>
2024-05-29 12:54:14 -0600

    se agrega el mensaje en el main

--- Array.java
+++ Array.java
@@ -1,6 +1,6 @@
 public class Array {
     public static void main(String[] args) {
     System.out.println("ArrayList");
-    
+    System.out.println("This is not array excercise, im learning how to use git...");
 }//PSVM
 }//Public class
